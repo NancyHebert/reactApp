fe9717b6ef1412be0750c7425762b9b133920917
nhebert <nhebert@uottawa.ca>
2019-12-13 10:53:00 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { Fragment } from 'react';
+//import ReactDOM from 'react-dom';
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//import MasterForm from './App.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// NAVIGATION
+import Nav from "./components/Nav";
+// PAGES
+import Home from "./pages/Home/HomePage";
+import About from "./pages/About/AboutPage";
+import Contact from "./pages/Contact/ContactPage";
+import Signup from "./pages/SignUp/SignUpPage"
+
+import './index.css';
+
+const App = () => (
+  <Fragment>
+    <Nav />
+    <div className="container">
+      <Route exact={true} path="/" component={Home} />
+      <Route exaxt path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/signup" component={Signup}/>
+    </div>
+  </Fragment>
+);
+
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/SignUp/SignUpPage.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import SignUpForm from "../../components/SignUpForm";
+
+const SignUpPage = () => (
+  <Fragment>
+    <h1>Register</h1>
+    <SignUpForm />
+  </Fragment>
+);
+
+export default SignUpPage;
+
+
